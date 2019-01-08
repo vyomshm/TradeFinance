b576626d6b6e1663da9ee4361bb4021421a0b16a
Vyom Sharma <vyomshm@gmail.com>
2019-01-08 21:42:27 +0530

    fixed contract builds

--- truffle.js
+++ truffle.js
@@ -1,7 +1,7 @@
 let path = require('path');
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "buyer/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
       host: 'localhost',
